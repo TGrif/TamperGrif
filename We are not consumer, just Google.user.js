24d48cc4289024574e2e7563a59e0487abc49ca7
Thomas Griffault <thomas.griffault@gmail.com>
2021-11-13 17:13:09 +0100

    Update script with Informations instead of Info

--- We are not consumer, just Google.user.js
+++ We are not consumer, just Google.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         We are not consumer, just Google
 // @namespace    http://direct-shoot.com/
-// @version      0.1
+// @version      0.2
 // @description  We are not consumers but users, update link on Google search footer
 // @author       TGrif
 // @match        https://www.google.com/*
@@ -11,8 +11,8 @@
 (function() {
     'use strict';
     let elems = document.querySelectorAll("*"),
-    res_fr = Array.from(elems).find(v => v.textContent === 'Info consommateurs'),
+    res_fr = Array.from(elems).find(v => v.textContent === 'Informations consommateurs'),
     res_en = Array.from(elems).find(v => v.textContent === 'Consumer Information');
-    if (res_fr) res_fr.innerText = 'Info utilisateurs';
+    if (res_fr) res_fr.innerText = 'Informations utilisateurs';
     if (res_en) res_en.innerText = 'User Information';
 })();
