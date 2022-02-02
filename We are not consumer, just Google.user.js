4f04394f3a288c345d64db618acd07ca89f395cb
Thomas Griffault <thomas.griffault@gmail.com>
2022-02-02 21:52:14 +0100

    Remove namespace

--- We are not consumer, just Google.user.js
+++ We are not consumer, just Google.user.js
@@ -1,16 +1,14 @@
 // ==UserScript==
 // @name         We are not consumer, just Google
-// @namespace    http://direct-shoot.com/
 // @version      0.2
 // @description  We are not consumers but users, update link on Google search footer
 // @author       TGrif
 // @match        https://www.google.com/*
-// @grant        none
 // ==/UserScript==
 
 (function() {
-    'use strict';
-    let elems = document.querySelectorAll("*"),
+    "use strict";
+    let elems = document.querySelectorAll('*'),
     res_fr = Array.from(elems).find(v => v.textContent === 'Informations consommateurs'),
     res_en = Array.from(elems).find(v => v.textContent === 'Consumer Information');
     if (res_fr) res_fr.innerText = 'Informations utilisateurs';
