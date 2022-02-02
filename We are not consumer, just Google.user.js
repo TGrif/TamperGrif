c1cf4e13fd838d80121d37669dd752c94213da40
Thomas Griffault <thomas.griffault@gmail.com>
2022-02-02 22:10:28 +0100

    Taking in count abreviation

--- We are not consumer, just Google.user.js
+++ We are not consumer, just Google.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         We are not consumer, just Google
-// @version      0.2
+// @version      0.3
 // @description  We are not consumers but users, update link on Google search footer
 // @author       TGrif
 // @match        https://www.google.com/*
@@ -9,7 +9,7 @@
 (function() {
     "use strict";
     let elems = document.querySelectorAll('*'),
-    res_fr = Array.from(elems).find(v => v.textContent === 'Informations consommateurs'),
+    res_fr = Array.from(elems).find(v => (v.textContent === 'Informations consommateurs' || v.textContent === 'Info consommateurs')),
     res_en = Array.from(elems).find(v => v.textContent === 'Consumer Information');
     if (res_fr) res_fr.innerText = 'Informations utilisateurs';
     if (res_en) res_en.innerText = 'User Information';
